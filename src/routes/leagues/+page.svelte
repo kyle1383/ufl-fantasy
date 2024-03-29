84321734bc9299d0df54909f27923ecfe773843e
Kyle Morand <kmorand@me.com>
2024-03-29 07:50:48 -0400

    removing nulls

--- src/routes/leagues/+page.svelte
+++ src/routes/leagues/+page.svelte
@@ -15,14 +15,13 @@
 
 	onMount(async () => {
 		invites = JSON.parse(window.localStorage.getItem('invites') || '[]');
+		invites = invites.filter((invite) => invite !== null)
+		removeLeagueFromInvites(null);
 		let invitesIds = invites.map((invite) => invite.split('/')[2]);
 		//remove invites from local storage if league i in user_leagues
 		invitesIds = invitesIds.filter((id) => {
 			// Log the first user league for debugging purposes
-			if (id === null ){
-				removeLeagueFromInvites(id)
-				return false;
-			}
+			
 			// Use `some` to check if `user_leagues` contains an element with a matching `id`
 			if (!user_leagues.some((l) => l.id.toString() === id)) {
 				return true; // Keep the id in `invitesIds` if it's not found in `user_leagues`
