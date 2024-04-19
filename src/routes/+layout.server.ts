ee3861bd9037773feead822247de26c3c77e90d6
Kyle Morand <kmorand@me.com>
2024-04-19 10:15:32 -0600

    logrocket v2

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,17 +1,8 @@
 import type { LayoutServerLoad } from './$types'
-import LogRocket from 'logrocket';
+
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
   const session = await getSession()
-  if (session && session.user) {
-    LogRocket.identify(session.user.id, {
-      name: session.user.full_name,
-      email: session.user.email,
-    
-      // Add your own custom user variables here, ie:
-      subscriptionType: 'pro'
-    });
-  }
   return {
-    session:session
+    session: await getSession(),
   }
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
+	import { page } from '$app/stores';
 	import '../app.css';
 	import LogRocket from 'logrocket';
 	LogRocket.init('1zkmlx/ufl-fantasy');
+	$: user = $page.data.session.user;
+	if(user){
+		console.log('adding to ler')
+		LogRocket.identify(user.id, {
+		name: user.full_name,
+		email: user.email,
+	});
+	}
+	
 </script>
 
 <slot />
